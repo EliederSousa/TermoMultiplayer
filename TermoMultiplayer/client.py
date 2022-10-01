1edf82f4e7a789c5853720c314cce561e4e25504
Elieder Damasceno Sousa <eliedersousa@gmail.com>
2022-10-01 19:56:50 -0300

    Update client.py

--- TermoMultiplayer/client.py
+++ TermoMultiplayer/client.py
@@ -7,22 +7,12 @@ import threading
 import fsm
 
 
-"""            //=======\\
-               ||       ||
-               \/       ||
-> login ==> gameloop    ||
-    ||         ||       ||
-    ||         \/       ||
-    \\====> waiting ====// """
-
-
 fsm = fsm.FiniteStateMachine()
 fsm.createState("login", ["gameloop", "wait"])
 fsm.createState("gameloop", ["wait"])
 fsm.createState("wait", ["gameloop"])
 fsm.changeState("login")
 state = fsm.getState()
-
 
 class ClienteInfo:
     nome = ""
@@ -43,7 +33,7 @@ clientSocket = ""
 waitingInfo = ""
 
 # Checa se a entrada é valida. 
-def entry_checkLetter( ev, widget, linha ):
+def entry_checkLetter( ev, widget, linha, nextBox, prevBox ):
     if ev.keysym.isalpha():
         widget.delete(1, 'end')
         text = widget.get()
@@ -51,19 +41,20 @@ def entry_checkLetter( ev, widget, linha ):
             text = text.upper()
             widget.delete(0, 'end')
             widget.insert(0, text)
+            nextBox.focus_set()
             if len(cells[linha][0].get()) > 0 and len(cells[linha][1].get()) > 0 and len(cells[linha][2].get()) > 0 and len(cells[linha][3].get()) > 0 and len(cells[linha][4].get()) > 0:
                 palavra = cells[linha][0].get() + cells[linha][1].get()  + cells[linha][2].get()  + cells[linha][3].get()  + cells[linha][4].get() 
                 enviarPalavra(palavra)
-        else:
+        elif ev.keysym.upper() == "BACKSPACE":
+            print("teste")
             widget.delete(0, 'end')
+            prevBox.focus_set()
 
 def enviarPalavra( palavra ):
     client.text = palavra
     client.info = "gameloop"
     sock.send(pickle.dumps(client))
-    
-    #data, addr = sock.recvfrom(2048)
-    #data = pickle.loads(data)
+
 
 def entry_checkName( ev, widget ):
     widget.delete(10, 'end')
@@ -75,7 +66,7 @@ def entry_checkName( ev, widget ):
 # Função que cia uma thread e possibilita o recebiment ode mensagens do servidor.
 # Com ela, o servidor sempre tem uma confirmação de que o cliente está logado e não dá erro de desconexão.
 def clientThread():
-    global guesses, waitingForNewRow, fsm, waitingInfo
+    global guesses, waitingForNewRow, fsm, waitingInfo, timeMsgToNextWord
     print("Thread criada para o cliente {}".format(client.addr))
     while True:
         data = sock.recv(2048)
@@ -86,15 +77,14 @@ def clientThread():
                 fsm.changeState("gameloop")
             else:
                 waitingInfo = data.info
-            
+        
+        if data.code == 700:
+            timeMsgToNextWord = data.info
         if data.code == 900:
             guesses = data.info
             waitingForNewRow = True
         elif data.code == 910 and not (fsm.getState() == "wait") :
             fsm.changeState("wait")
-        #print(data.info)
-        #client.info = "ping"
-        #sock.send(pickle.dumps(client))
         
 
 def btn_login_callback( nome, ip ):
@@ -139,27 +129,28 @@ def createrow( placeholder, n ):
     # Caixas de texto
     # Não funciona dentro de um loop. Dá conflito nos nomes dos widgets.
     btn1 = tk.Entry(frame, width=2, justify = 'center', font=('consolas', 30, 'bold') )
-    btn1.bind("<KeyRelease>", lambda event: entry_checkLetter(event, btn1, n))
+    btn1.bind("<KeyRelease>", lambda event: entry_checkLetter(event, btn1, n, btn2, btn5))
     btn1.pack(side='left')
+    btn1.focus_set()
     temp.append(btn1)
 
     btn2 = tk.Entry(frame, width=2, justify = 'center', font=('consolas', 30, 'bold') )
-    btn2.bind("<KeyRelease>", lambda event: entry_checkLetter(event, btn2, n))
+    btn2.bind("<KeyRelease>", lambda event: entry_checkLetter(event, btn2, n, btn3, btn1))
     btn2.pack(side='left')
     temp.append(btn2)
 
     btn3 = tk.Entry(frame, width=2, justify = 'center', font=('consolas', 30, 'bold') )
-    btn3.bind("<KeyRelease>", lambda event: entry_checkLetter(event, btn3, n))
+    btn3.bind("<KeyRelease>", lambda event: entry_checkLetter(event, btn3, n, btn4, btn2))
     btn3.pack(side='left')
     temp.append(btn3)
 
     btn4 = tk.Entry(frame, width=2, justify = 'center', font=('consolas', 30, 'bold') )
-    btn4.bind("<KeyRelease>", lambda event: entry_checkLetter(event, btn4, n))
+    btn4.bind("<KeyRelease>", lambda event: entry_checkLetter(event, btn4, n, btn5, btn3))
     btn4.pack(side='left')
     temp.append(btn4)
 
     btn5 = tk.Entry(frame, width=2, justify = 'center', font=('consolas', 30, 'bold') )
-    btn5.bind("<KeyRelease>", lambda event: entry_checkLetter(event, btn5, n))
+    btn5.bind("<KeyRelease>", lambda event: entry_checkLetter(event, btn5, n, btn1, btn4))
     btn5.pack(side='left')
     temp.append(btn5)
 
@@ -185,33 +176,31 @@ def colorRow( n, guesses ):
 
 def login():
     root = tk.Tk()
-    root.geometry("300x100")
-    root.title('CONECTE AO TERMO')
+    root.geometry("340x100")
+    root.title('CONECTE-SE AO TERMO')
     root.resizable(0, 0)
-    # root.configure(background = 'black') #Caso essa linha dê problema há uma possivel solução abaixo
-    #https://stackoverflow.com/questions/10887762/python-tkinter-root-window-background-configuration
     
     # GRID
     root.columnconfigure(0, weight=1)
     root.columnconfigure(1, weight=3)
 
     # Apelido
-    username_label = ttk.Label(root, text="Seu apelido (até 10 letras):")
+    username_label = tk.Label(root, text="Seu apelido (até 10 letras):", fg="red", font=("Consolas", 10, "bold"))
     username_label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5) 
-    username_entry = ttk.Entry(root)
+    username_entry = tk.Entry(root)
     username_entry.grid(column=1, row=0, sticky=tk.E, padx=5, pady=5)
     username_entry.bind("<KeyRelease>", lambda event: entry_checkName(event, username_entry))
     username_entry.insert(0, "Elieder")
 
     # IP
-    IP_label = ttk.Label(root, text="IP do servidor:")
+    IP_label = tk.Label(root, text="IP do servidor:", fg="red", font=("Consolas", 10, "bold"))
     IP_label.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
-    IP_entry = ttk.Entry(root)
+    IP_entry = tk.Entry(root)
     IP_entry.insert(0, "192.168.1.105")
     IP_entry.grid(column=1, row=1, sticky=tk.E, padx=5, pady=5)
 
     # Botão
-    login_button = ttk.Button(root, text="CONECTAR!", \
+    login_button = tk.Button(root, text="CONECTAR!", \
         command= lambda: btn_login_callback(username_entry.get(), IP_entry.get()))
     login_button.grid(column=1, row=3, sticky=tk.E, padx=5, pady=5)
 
@@ -226,22 +215,28 @@ def login():
     gameloop()
 
 waitingForNewRow = False
+timeMsgToNextWord = ""
 tries = 0
 guesses = ""
 
 def gameloop():
-    global cells, waitingForNewRow, tries, guesses, waitingInfo
+    global cells, waitingForNewRow, tries, guesses, waitingInfo, timeMsgToNextWord
 
     tries = 0
     
     root = tk.Tk()
-    root.geometry('400x500')
+    root.geometry("400x500")
     root.title(client.nome)
+
+    info_label = tk.Label(root, font=("Consolas", 13, "bold"))
+    info_label.pack()
     
     cells = []
     createrow(root, tries)
 
     while True:
+        info_label.config(text=timeMsgToNextWord)
+
         if waitingForNewRow:
             waitingForNewRow = False
             tries += 1
@@ -263,35 +258,22 @@ def gameloop():
     
 def wait():
     global waitingInfo
-    """
-    for m in cells:
-        for n in m:
-            n.unbind()"""
     
     root = tk.Tk()
-    root.geometry("300x300")
-    root.title('Aguarde a próxima rodada')
+    root.geometry("400x200")
+    root.title("Aguarde a próxima rodada")
     root.resizable(0, 0)
-    # root.configure(background = 'black') #Caso essa linha dê problema há uma possivel solução abaixo
-    #https://stackoverflow.com/questions/10887762/python-tkinter-root-window-background-configuration
-    
-    # GRID
 
-    # Apelido
-    info_label = ttk.Label(root)
+    info_label = ttk.Label(root, font=("Consolas", 13, "bold"))
     info_label.pack()
     
     while True:
-        # não fazer tk.mainloop() pois ele bloqueia a execução.
-        # use um loop infinito, e chame root.update()
         info_label.config(text=waitingInfo)
-        
         root.update()
         if fsm.getState() == "gameloop":
             break
     
     root.destroy()
     gameloop()
-
 
 login()
